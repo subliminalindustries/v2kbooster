c467546e018a41a18c51fd5afb1f4bf85c87d33a
Subliminal Industries <subliminalindustries@pm.me>
2023-06-24 10:43:25 +0200

    Fix formatting.

--- v2kbooster.py
+++ v2kbooster.py
@@ -8,7 +8,8 @@ parser = argparse.ArgumentParser(prog='v2kbooster',
                                  description='Boost v2k recordings to an intelligible level')
 
 parser.add_argument('-f', '--fftn', required=False, type=int, help='Number of FFT bins (ex.: 8192)', default=8192)
-parser.add_argument('-w', '--weights', required=False, type=str, nargs='+', help='List of harmonic weights (ex.: 1. .5 .33 .25 .165)', default=[1., .5, .33, .25, .165])
+parser.add_argument('-w', '--weights', required=False, type=str, nargs='+',
+                    help='List of harmonic weights (ex.: 1. .5 .33 .25 .165)', default=[1., .5, .33, .25, .165])
 parser.add_argument('pattern', type=str, help='File glob pattern (ex.: "./*.wav")')
 
 args = parser.parse_args(sys.argv[1:])
