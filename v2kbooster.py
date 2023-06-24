24697917831815bb882fee3e8df9056004206bbf
Subliminal Industries <subliminalindustries@pm.me>
2023-06-24 05:43:09 +0200

    Added --fftn switch

--- v2kbooster.py
+++ v2kbooster.py
@@ -9,7 +9,8 @@ parser = argparse.ArgumentParser(prog='v2kbooster',
                                  description='Boost v2k recordings to an intelligible level')
 
 parser.add_argument('pattern', type=str, help='Glob pattern (ex.: "./*.wav")')
+parser.add_argument('--fftn', type=int, help='FFT N', default=8192)
 
 args = parser.parse_args(sys.argv[1:])
 
-v2kbooster.process(args.pattern)
+v2kbooster.process(args.pattern, nfft=args.fftn)
