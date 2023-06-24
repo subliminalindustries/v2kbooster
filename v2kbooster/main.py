904db3d5fbcf0d680d080198ff6d6099fef0148c
Subliminal Industries <subliminalindustries@pm.me>
2023-06-24 14:58:11 +0200

    Split large files into chunk for processing to prevent resource issues.

--- v2kbooster/main.py
+++ v2kbooster/main.py
@@ -5,6 +5,8 @@ import librosa
 import numpy as np
 import os
 import pyloudnorm
+import sys
+import traceback
 import warnings
 
 from glob import glob
@@ -14,8 +16,11 @@ from soundfile import LibsndfileError
 
 warnings.filterwarnings('ignore', category=UserWarning)
 
+MAX_CHUNK_SIZE = 50000000
 
-def load_file(filename: str, mono: bool = False) -> (np.ndarray, int):
+
+def load_file(filename: str,
+              mono: bool = False) -> (np.ndarray, int):
     try:
         signal, fs = audiofile.read(filename)
         if mono and len(signal.shape) == 2:
@@ -30,7 +35,9 @@ def interpolate(data: np.ndarray, to_size: int) -> np.ndarray:
     return np.interp(np.linspace(0, data.size - 1, num=to_size), np.arange(data.size), data)
 
 
-def hilbert_frequency_rms(data: np.ndarray, fs: int, window_size: int = 16) -> np.ndarray:
+def hilbert_frequency_rms(data: np.ndarray,
+                          fs: int,
+                          window_size: int = 16) -> np.ndarray:
     w = min(64, max(16, window_size // 32))
     f = (np.diff(np.unwrap(np.angle(hilbert(data)))) / (2.0 * np.pi) * fs)
 
@@ -38,25 +45,15 @@ def hilbert_frequency_rms(data: np.ndarray, fs: int, window_size: int = 16) -> n
     for x in range(0, f.size - w, w):
         mf.append(np.sqrt(np.mean(f[x:x + w]) ** 2))
 
-    return 1 - savgol_filter(np.array(mf), w, 1)
+    return 1-savgol_filter(np.array(mf), w, 1)
 
 
-def enhance_salient(data: np.ndarray,
-                    fs: int,
-                    weights: list,
-                    nfft: int,
-                    hw: float) -> np.ndarray:
-    harmonics = list(np.arange(1, len(weights)+1))
-
-    harmonic_weights = ', '.join(list(map(lambda x: f'{x[0]}={x[1]}', zip(harmonics, weights))))
-
-    print(f'harmonic weights: {harmonic_weights}')
-    print(f'fft bins: {nfft}')
-
-    meter = pyloudnorm.Meter(fs)
-    loudness = meter.integrated_loudness(data)
-    data = pyloudnorm.normalize.loudness(data, loudness, -1.0)
-
+def enhance_harmonic_saliency(data: np.ndarray,
+                              fs: int,
+                              harmonics: list,
+                              weights: list,
+                              nfft: int,
+                              hw: float) -> np.ndarray:
     spectrum = np.abs(librosa.stft(data, n_fft=nfft))
 
     spectrum = spectrum * (.5 * librosa.salience(spectrum,
@@ -72,16 +69,14 @@ def enhance_salient(data: np.ndarray,
 
     signal = interpolate(signal, data.size) * envelope
 
-    hp_filter = pyloudnorm.IIRfilter(0.0, 0.7, 80.0, fs, 'high_pass')
-    meter = pyloudnorm.Meter(fs, 'DeMan')
-    meter._filters.__setitem__('hp_filter', hp_filter)
-    loudness = meter.integrated_loudness(signal)
-    signal = pyloudnorm.normalize.loudness(signal, loudness, -1.0)
-
     return signal
 
 
-def process_file(filename: str, weights: list, nfft: int, hw: float, overwrite: bool) -> bool:
+def process_file(filename: str,
+                 weights: list,
+                 nfft: int,
+                 hw: float,
+                 overwrite: bool) -> bool:
     if filename.find('-enhanced.wav') != -1:
         return False
 
@@ -99,38 +94,79 @@ def process_file(filename: str, weights: list, nfft: int, hw: float, overwrite: 
 
     print(f'> processing "{filename}"..')
 
+    harmonics = list(np.arange(1, len(weights)+1))
+    harmonic_weights = ', '.join(list(map(lambda x: f'{x[0]}={x[1]}', zip(harmonics, weights))))
+
+    print(f'harmonic weights: {harmonic_weights}')
+    print(f'fft bins: {nfft}')
+
     signal, fs = load_file(filename, mono=True)
-    enhanced = minmax_scale(enhance_salient(signal, fs, weights, nfft, hw), (-1., 1.))
+    print(f'samples: {len(signal)}')
+
+    meter = pyloudnorm.Meter(fs)
+    loudness = meter.integrated_loudness(signal)
+    signal = pyloudnorm.normalize.loudness(signal, loudness, -10.0)
+    signal = np.nan_to_num(signal, posinf=1., neginf=-1.)
+
+    samples = len(signal)
+    chunks = len(signal) // MAX_CHUNK_SIZE
+    samples -= (chunks * MAX_CHUNK_SIZE)
+    if samples:
+        chunks += 1
+
+    parts = []
+    for x in range(0, chunks):
+        rem = min(len(signal), MAX_CHUNK_SIZE)
+        if rem:
+            parts.append(signal[:rem-1])
+            signal = signal[rem:]
+
+    if len(parts) > 1:
+        print(f'! large file, broken up into {len(parts)} chunks to prevent resource issues.')
+
+    for k, part in enumerate(parts):
+        if len(parts) > 1:
+            print(f'> chunk {k+1}..', end='\r')
+        parts[k] = minmax_scale(enhance_harmonic_saliency(part, fs, harmonics, weights, nfft, hw), (-1., 1.))
+
+    enhanced = np.block(parts)
+
+    hp_filter = pyloudnorm.IIRfilter(0.0, 0.7, 80.0, fs, 'high_pass')
+    meter = pyloudnorm.Meter(fs, 'DeMan')
+    meter._filters.__setitem__('hp_filter', hp_filter)
+    loudness = meter.integrated_loudness(enhanced)
+    enhanced = pyloudnorm.normalize.loudness(enhanced, loudness, -10.0)
 
     audiofile.write(file=new_filename, signal=enhanced, sampling_rate=fs)
 
-    print(f'> wrote "{new_filename}"\n')
+    print(f'> wrote "{new_filename}"\n{"-" * 80}')
 
     return True
 
 
-def process(pattern: str, weights: list = None, nfft: int = 8192, envelope_weight: float = 1., overwrite: bool = False):
+def process(pattern: str,
+            weights: list = None,
+            nfft: int = 8192,
+            envelope_weight: float = 1.,
+            overwrite: bool = False) -> None:
     if weights is None:
         weights = [1., .5, .33, .25, .165]
     elif type(weights) is list:
         weights = list(map(lambda x: min(1., float(x)), weights))
 
     files = sorted(glob(pattern, recursive=True))
-    if len(files):
-        print(f'found {len(files)} files.')
+    print(f'found {len(files)} files.\n{"-" * 80}')
 
-        done_work = False
+    done_work = False
 
-        for file in files:
-            try:
-                if process_file(file, weights, nfft, envelope_weight, overwrite) and done_work is False:
-                    done_work = True
-            except Exception as e:
-                print(e)
+    for file in files:
+        try:
+            if process_file(file, weights, nfft, envelope_weight, overwrite) and done_work is False:
+                done_work = True
+        except Exception as e:
+            print(''.join(traceback.format_exception(sys.exc_info()[0], sys.exc_info()[1], sys.exc_info()[2])))
 
-        if done_work:
-            print('done.')
-        else:
-            print('nothing to do.')
+    if done_work:
+        print('done.')
     else:
-        print('no files to process')
+        print('nothing to do.')
