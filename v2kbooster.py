1aa6c9ee8df508a6f20f4322bb780ede776224b3
Subliminal Industries <subliminalindustries@pm.me>
2023-06-24 12:39:14 +0200

    Add --overwrite flag.

--- v2kbooster.py
+++ v2kbooster.py
@@ -7,11 +7,13 @@ import v2kbooster
 parser = argparse.ArgumentParser(prog='v2kbooster',
                                  description='Boost v2k recordings to an intelligible level')
 
-parser.add_argument('-f', '--fftn', required=False, type=int, help='Number of FFT bins (ex.: 8192)', default=8192)
+parser.add_argument('-o', '--overwrite', required=False, type=bool,
+                    help='Whether to overwrite existing files (default: False)', default=False)
+parser.add_argument('-f', '--fftn', required=False, type=int, help='Number of FFT bins (default: 8192)', default=8192)
 parser.add_argument('-w', '--weights', required=False, type=str, nargs='+',
-                    help='List of harmonic weights (ex.: 1. .5 .33 .25 .165)', default=[1., .5, .33, .25, .165])
+                    help='List of harmonic weights (default: 1. .5 .33 .25 .165)', default=[1., .5, .33, .25, .165])
 parser.add_argument('pattern', type=str, help='File glob pattern (ex.: "./*.wav")')
 
 args = parser.parse_args(sys.argv[1:])
 
-v2kbooster.process(args.pattern, nfft=args.fftn, weights=args.weights)
+v2kbooster.process(args.pattern, nfft=args.fftn, weights=args.weights, overwrite=args.overwrite)
