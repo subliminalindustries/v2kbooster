68f2b37f798366fb10faa7c20c7d6dd97bacc330
Subliminal Industries <subliminalindustries@pm.me>
2023-06-24 10:34:55 +0200

    Add more helpful argument descriptions.

--- v2kbooster.py
+++ v2kbooster.py
@@ -2,15 +2,14 @@
 
 import sys
 import argparse
-
 import v2kbooster
 
 parser = argparse.ArgumentParser(prog='v2kbooster',
                                  description='Boost v2k recordings to an intelligible level')
 
-parser.add_argument('--fftn', required=False, type=int, help='FFT N', default=8192)
-parser.add_argument('--weights', required=False, type=str, nargs='+', help='Harmonic weights', default=[1., .5, .33, .25, .165])
-parser.add_argument('pattern', type=str, help='Glob pattern (ex.: "./*.wav")')
+parser.add_argument('--fftn', required=False, type=int, help='Number of FFT bins (ex.: 8192)', default=8192)
+parser.add_argument('--weights', required=False, type=str, nargs='+', help='List of harmonic weights (ex.: 1. .5 .33 .25 .165)', default=[1., .5, .33, .25, .165])
+parser.add_argument('pattern', type=str, help='File glob pattern (ex.: "./*.wav")')
 
 args = parser.parse_args(sys.argv[1:])
 
